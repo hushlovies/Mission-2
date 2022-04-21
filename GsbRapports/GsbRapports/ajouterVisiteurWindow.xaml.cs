b389ff89da25251af3062119dd4c37d1f41e4c8b
hushlovies <canariakaira@gmail.com>
2022-04-21 14:03:06 +0200

    ajout

--- GsbRapports/GsbRapports/ajouterVisiteurWindow.xaml.cs
+++ GsbRapports/GsbRapports/ajouterVisiteurWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Newtonsoft.Json;
+using System.Collections.Specialized;
 
 namespace GsbRapports
 {
@@ -31,15 +33,35 @@ namespace GsbRapports
             this.laSecretaire = laSecretaire;
             this.wb = wb;
             this.site = site;
-        }       
-        private void btnValider_Click(object sender, RoutedEventArgs e)
-        {
-
         }
 
-        private void btnAnnuler_Click(object sender, RoutedEventArgs e)
+        private void btnValider_Click1(object sender, RoutedEventArgs e)
         {
+            
+                try
+                {
+                    string url = this.site + "visiteurs";
+                    NameValueCollection parametres = new NameValueCollection();
+                    parametres.Add("ticket", this.laSecretaire.getHashTicketMdp());
+                    parametres.Add("idVisiteur", this.txtIdVisiteur.Text);
+                    parametres.Add("nom", this.txtNom.Text);
+                    parametres.Add("prenom", this.txtPrenom.Text);
+                    parametres.Add("adresse", this.txtAdresse.Text);
+                    parametres.Add("ville", this.txtVille.Text);
+                    parametres.Add("cp", this.txtCodePostale.Text);
+                    parametres.Add("dateEmbauche", this.txtDateEmbauche.Text);
+                    byte[] tabByte = wb.UploadValues(url, parametres); // envoie des donnés en post 
+                    string ticket = UnicodeEncoding.UTF8.GetString(tabByte);
+                    this.laSecretaire.ticket = ticket.Substring(2);// anti slash n 
+                    MessageBox.Show("Ajout d'un visiteur effectué");
 
+                }
+                catch (WebException ex)
+                {
+                    if (ex.Response is HttpWebResponse)
+                        MessageBox.Show(((HttpWebResponse)ex.Response).StatusCode.ToString());
+                }
+            
         }
     }
 }
